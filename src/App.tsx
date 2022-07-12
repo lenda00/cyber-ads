6ffc369c8e3caf6f7e5851c7731990abe8242e9f
kewerson hugo <kewerson.hugo@gmail.com>
2022-07-11 23:40:03 -0300

    Add slider prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Slider from "react-slick";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      arrows: false,
+      infinite: true,
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 10000,
+      fade: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div style={{
+        width: "100%",
+        height: "100%"
+      }}>
+        <Slider {...settings}>
+          {[1,2,3].map(n => (
+          <div>
+            <img src={`cyber-ads/images/cyber-${n}.jpg`} alt={`slide-${n}`}/>
+          </div>
+          ))}
+        </Slider>
+      </div>
+    );
+  }
 }
-
-export default App;
